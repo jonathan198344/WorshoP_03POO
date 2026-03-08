78a92c5cf2e2282ee14cbdd7fada5455ac316b09
jonathan198344 <jonathan_-1983@outlook.com>
2026-03-08 00:28:22 -0500

    Worshop

---
+++ Worshop/Matrixmultiplication/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m, n, p;
+
+        Console.Write("Ingrese el valor de m: ");
+        m = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de n: ");
+        n = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de p: ");
+        p = int.Parse(Console.ReadLine());
+
+        int[,] A = new int[m, n];
+        int[,] B = new int[n, p];
+        int[,] C = new int[m, p];
+
+        // Llenar matriz A
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                A[i, j] = (i + 1) * j;
+            }
+        }
+
+        // Llenar matriz B
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                B[i, j] = (j + 1) * i;
+            }
+        }
+
+        // Multiplicación de matrices A x B
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                C[i, j] = 0;
+
+                for (int k = 0; k < n; k++)
+                {
+                    C[i, j] += A[i, k] * B[k, j];
+                }
+            }
+        }
+
+        // Mostrar matriz A
+        Console.WriteLine("\n*** A ***");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(A[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Mostrar matriz B
+        Console.WriteLine("\n*** B ***");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                Console.Write(B[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Mostrar matriz C
+        Console.WriteLine("\n*** C ***");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                Console.Write(C[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Worshop/Rhombus/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño del rombo: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int mitad = n / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            int espacios = Math.Abs(mitad - i);
+            int ancho = n - 2 * espacios;
+
+            // Espacios antes del rombo
+            for (int j = 0; j < espacios; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Imprimir los #
+            if (ancho == 1)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write("#");
+
+                for (int j = 0; j < ancho - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("#");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
